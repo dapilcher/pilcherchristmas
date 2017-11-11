2860ab2a90cda51d6f09b9a3735200baf824ec18
Doug Pilcher <dpilcher.dev@gmail.com>
2017-11-10 18:40:29 -0600

    just some stuff

--- config.rb
+++ config.rb
@@ -58,4 +58,5 @@ end
 configure :build do
   activate :minify_css
   activate :minify_javascript
+  activate :asset_hash
 end
--- source/javascripts/_site.js
+++ source/javascripts/_site.js
@@ -1,6 +1,4 @@
 // This is where it all goes :)
-
-
 const shiny = document.querySelectorAll('.shiny');
 
 let hue = 0;
@@ -10,4 +8,4 @@ setInterval(function () {
     item.setAttribute('style', `text-shadow: 0px 0px 2rem hsl(${hue}, 100%, 50%)`);
   })
   hue > 358 ? hue = 0 : hue++;
-}, 10)
+}, 10);
